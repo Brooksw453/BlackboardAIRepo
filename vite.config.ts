86d8d00ab3b1465322b5410408d7b86a37d3d2b9
Brooks Winchell <122840510+Brooksw453@users.noreply.github.com>
2024-01-20 13:54:59 -0500

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        outDir: "static", // Output directory set to 'static' in the root
+        emptyOutDir: true,
+        sourcemap: true
+    },
+    server: {
+        proxy: {
+            "/ask": "http://localhost:5000",
+            "/chat": "http://localhost:5000"
+        }
+    }
+});
+
